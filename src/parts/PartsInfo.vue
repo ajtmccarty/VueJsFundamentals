0aceaa0ecc282b4cd909b5e63c29d9ef619b3cf4
ajtmccarty <amccarty@cloudbolt.io>
2020-04-12 15:47:36 -0700

    Update other views to use new API action on Vuex store for parts information

--- src/parts/PartsInfo.vue
+++ src/parts/PartsInfo.vue
@@ -8,10 +8,11 @@
 </template>
 
 <script>
-import parts from '../data/parts';
+import getPartsMixin from './get-parts-mixin';
 
 export default {
   name: 'PartInfo',
+  mixins: [getPartsMixin],
   props: {
     partType: {
       type: String,
@@ -25,7 +26,7 @@ export default {
   },
   computed: {
     part() {
-      return parts[this.partType].find((part) => part.id === +this.partId);
+      return this.parts[this.partType].find((part) => part.id === +this.partId);
     },
   },
 };
