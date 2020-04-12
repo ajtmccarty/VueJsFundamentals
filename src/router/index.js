247950e3fe31cd104218420831bdf8c8977a95df
ajtmccarty <amccarty@cloudbolt.io>
2020-04-12 09:09:19 -0700

    Add routes for HomePage and RobotBuilder

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import HomePage from '../home/HomePage.vue';
+import RobotBuilder from '../build/RobotBuilder.vue';
+
 Vue.use(Router);
 
 export default new Router({
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomePage,
+    },
+    {
+      path: '/build',
+      name: 'Buile',
+      component: RobotBuilder,
+    },
+  ],
 });
