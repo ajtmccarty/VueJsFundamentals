1b9940d8622e35f586b85f34d0f75405392be058
ajtmccarty <amccarty@cloudbolt.io>
2020-04-12 11:02:28 -0700

    Add BrowseParts Component and Route

---
+++ src/parts/BrowseParts.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Browse Parts</h1>
+    <ul class="menu">
+      <li>
+        <router-link :to="{name: 'BrowseHeads'}">Head</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'BrowseArms'}">Arm</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'BrowseTorsos'}">Torso</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'BrowseBases'}">Base</router-link>
+      </li>
+    </ul>
+    <router-view>
+
+    </router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BrowseParts',
+};
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+  .menu {
+    display: flex;
+    justify-content: space-around;
+    margin: 0 100px;
+    font-size: 20px;
+  }
+  ul {
+    list-style-type: none;
+  }
+  a {
+    color: palevioletred;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router';
 
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotBases from '../parts/RobotBases.vue';
 import PartsInfo from '../parts/PartsInfo.vue';
 
 Vue.use(Router);
@@ -18,6 +23,33 @@ export default new Router({
       path: '/build',
       name: 'Build',
       component: RobotBuilder,
+    },
+    {
+      path: '/parts/browse',
+      name: 'BrowseParts',
+      component: BrowseParts,
+      children: [
+        {
+          name: 'BrowseHeads',
+          path: 'heads',
+          component: RobotHeads,
+        },
+        {
+          name: 'BrowseArms',
+          path: 'arms',
+          component: RobotArms,
+        },
+        {
+          name: 'BrowseTorsos',
+          path: 'torsos',
+          component: RobotTorsos,
+        },
+        {
+          name: 'BrowseBases',
+          path: 'Bases',
+          component: RobotBases,
+        },
+      ],
     },
     {
       path: '/parts/:partType/:partId',
